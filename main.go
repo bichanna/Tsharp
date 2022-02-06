7630ce2ab9f6f3f5db22492c4a36f1f28c857add
“tsharp0x11” <ibuki042004@gmail.com>
2022-02-06 18:18:24 +0900

    added comments

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-
-/*
-
-Lexer (parse a list of characters and convert them to tokens.)
-↓
-Parser (converts a list of tokens into an AST)
-↓
-Visitor (visit AST node.)
-
-*/
-
-
 import (
 	"fmt"
 	"os"
